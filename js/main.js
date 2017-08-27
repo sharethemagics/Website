9d81cbb4fe521f422a53234ee02e5aacf541bf6c
Sathees Rajagopal <sharethemagics@gmail.com>
2017-08-28 00:40:47 +0530

    08/28 copy

--- js/main.js
+++ js/main.js
@@ -5,6 +5,29 @@ jQuery(function($) {
 		preloader.remove();
 	});
 	
+	//smooth sroll
+	$("a").on('click', function(event) {
+		
+				// Make sure this.hash has a value before overriding default behavior
+				if (this.hash !== "") {
+					// Prevent default anchor click behavior
+					event.preventDefault();
+		
+					// Store hash
+					var hash = this.hash;
+		
+					// Using jQuery's animate() method to add smooth page scroll
+					// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+					$('html, body').animate({
+						scrollTop: $(hash).offset().top
+					}, 800, function(){
+			 
+						// Add hash (#) to URL when done scrolling (default click behavior)
+						window.location.hash = hash;
+					});
+				} // End if
+	});
+
 	//#main-slider
 	var slideHeight = $(window).height();
 	$('#home-slider .item').css('height',slideHeight);
@@ -39,8 +62,8 @@ if(hash !== '#home') {
     } else {
       $('header nav').removeClass('inbody');
     }
-  });
-
+	});
+	
 
 	// Contact form
 	var form = $('#main-contact-form');
@@ -82,5 +105,6 @@ if(hash !== '#home') {
 	}
 	google.maps.event.addDomListener(window, 'load', initialize_map);
 	
+	new WOW().init();
 });
 
