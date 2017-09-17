b44dc565bf3058180fab75e986df5a4dc40ac779
Sathees Rajagopal <sharethemagics@gmail.com>
2017-09-17 16:13:16 +0530

    web-dev 09/17

--- src/js/webpack/home.js
+++ src/js/webpack/home.js
@@ -3,7 +3,7 @@ import '../../css/home.css';
 import '../main.js';
 
 
-import '../../images/slider/carousel_1.jpg';
-import '../../images/slider/carousel_2.jpg';
-import '../../images/slider/carousel_3.jpg';
+import '../../images/home/carousel_1.jpg';
+import '../../images/home/carousel_2.jpg';
+import '../../images/home/carousel_3.jpg';
 
